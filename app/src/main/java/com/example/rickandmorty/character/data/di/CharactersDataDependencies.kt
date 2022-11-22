77e707087ec7f3657897c707966b4f1c5007eb22
David Muñoz <david.munoz@col.vueling.com>
2022-11-22 23:11:57 +0100

    feat: Dagger2 dependencies added to the project

--- app/src/main/java/com/example/rickandmorty/character/data/di/CharactersDataDependencies.kt
+++ app/src/main/java/com/example/rickandmorty/character/data/di/CharactersDataDependencies.kt
@@ -1,2 +1,19 @@
 package com.example.rickandmorty.character.data.di
 
+import com.example.rickandmorty.data.model.utils.getRetrofitInstance
+import dagger.Module
+import dagger.Provides
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+class CharactersDataModule() {
+
+    @Provides
+    fun provideRetrofit(converterFactory: Converter.Factory): Retrofit =
+        getRetrofitInstance(converterFactory = converterFactory)
+
+    @Provides
+    fun provideGsonConverterFactory(): Converter.Factory = GsonConverterFactory.create()
+}
