ec4a6cd8bc4f3438cda7f0136c87f49b02e3431a
David Muñoz <david.munoz@col.vueling.com>
2022-11-29 21:25:23 +0100

    fix: refactor common feature to mvvm arch

--- app/src/main/java/com/example/rickandmorty/common/data/model/CommonDto.kt
+++ app/src/main/java/com/example/rickandmorty/common/data/model/CommonDto.kt
@@ -1,2 +1,18 @@
 package com.example.rickandmorty.common.data.model
 
+data class InfoDto(
+    val count: Int,
+    val pages: Int,
+    val next: String,
+    val prev: String
+)
+
+data class OriginDto(
+    val name: String,
+    val url: String
+)
+
+data class LocationDto(
+    val name: String,
+    val url: String
+)
