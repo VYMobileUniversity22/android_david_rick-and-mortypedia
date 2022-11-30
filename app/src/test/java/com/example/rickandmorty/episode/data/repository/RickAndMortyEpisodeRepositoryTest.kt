2b720257c67d4b1180bf7cd85f400fccaab43543
David Muñoz <david.munoz@col.vueling.com>
2022-11-30 21:22:15 +0100

    feat: EpisodeRepositoryTest added

---
+++ app/src/test/java/com/example/rickandmorty/episode/data/repository/RickAndMortyEpisodeRepositoryTest.kt
@@ -0,0 +1,17 @@
+package com.example.rickandmorty.episode.data.repository
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+
+class RickAndMortyEpisodeRepositoryTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
